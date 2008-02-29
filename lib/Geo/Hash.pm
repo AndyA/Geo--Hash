4e86c4d307e78e52b88fd04ccd14bdca615c2d55
Andy Armstrong <andy@hexten.net>
2008-02-29 01:13:06 +0000

    Added

---
+++ lib/Geo/Hash.pm
@@ -0,0 +1,62 @@
+package Geo::Hash;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Geo::Hash - [One line description of module's purpose here]
+
+=head1 VERSION
+
+This document describes Geo::Hash version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use Geo::Hash;
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< somfunc >>
+
+=cut
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+Geo::Hash requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-geo-hash@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2008, Andy Armstrong C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
